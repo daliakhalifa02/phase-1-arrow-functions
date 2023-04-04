ae67ee8974c9d2e041f77b13c7ce9cf27448d1db
daliakhalifa02 <daliakhalifa02@gmail.com>
2023-04-04 03:08:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+// divide
+const divide = function(a,b){
+    a = 2000;
+    b = 100;
+    return a/b;
+}
+console.log(divide())
+// square
+const square = (n) => {
+    return n*n;
+}
+console.log(square(2))
+// add
+ const add = (i,j) =>{
+    return i+j;
+}
+console.log(add(3,4))
